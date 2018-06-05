2b37324100e3e40ff889ea0c1e09483c2613c602
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-06-05 09:58:54 -0400

    array of data

--- src/MappingArraysOfData.js
+++ src/MappingArraysOfData.js
@@ -7,19 +7,33 @@ class MappingArraysOfData extends Component {
     }
 
     componentWillMount(){
-        fetch('http://swapi.co/api/people/?format=json')
-            .then( response => response.json() )
+        fetch('https://swapi.co/api/people/?format=json')
+            .then( response => response.json())
             .then( ({results: items}) => this.setState({items}))
+    }
+
+    filter(e){
+        this.setState({filter: e.target.value})
     }
 
     render() {
         let items = this.state.items;
+        if(this.state.filter){
+            items = items.filter( item =>
+            item.name.toLowerCase()
+                .includes(this.state.filter.toLowerCase()))
+        }
         return (
             <div>
-                {items.map(item => <h4 >{item.name}</h4>)}
+                <input type="text"
+                       onChange={this.filter.bind(this)}/>
+                {items.map(item =>
+                    <Person key={item.name} person={item}/>)}
             </div>
         )
     }
 }
+
+const Person = (props) => <h4>{props.person.name}</h4>
 
 export default MappingArraysOfData;
